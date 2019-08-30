fc646d4afe2173f2f4eb28e1f5c219d8b02c8fe1
Ashok <ashokkumarm0007@gmail.com>
2019-08-30 09:35:14 -0700

    this file is commited

---
+++ src/main/java/acs/Acs.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package acs;
+import org.springframework.boot.SpringApplication;  
+import org.springframework.boot.autoconfigure.SpringBootApplication;  
+
+/**
+ * @author user
+ *
+ */
+@SpringBootApplication
+public class Acs {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(Acs.class, args);  
+		System.out.println("Have a Nice Day");
+		
+
+	}
+
+}
